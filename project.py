ea929377f811a4ac951f00971b49957e5cffc3e5
awadhesh16 <studrepro@gmail.com>
2026-04-08 23:18:44 +0530

    prepare for deployment

--- project.py
+++ project.py
@@ -59,4 +59,5 @@ def delete_task(id):
     return jsonify({"message": "Deleted"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
